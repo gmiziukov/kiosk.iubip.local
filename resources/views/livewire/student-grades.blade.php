8994a982df6fa2a32fc81a457bffa891bc23cf0c
N1ka <nikak0416@gmail.com>
2024-10-02 17:29:25 +0300

    Изменение страницы успеваемости

--- resources/views/livewire/student-grades.blade.php
+++ resources/views/livewire/student-grades.blade.php
@@ -5,12 +5,27 @@
         <img src="https://www.iubip.ru/local/templates/univer/img/logo.svg">
         <img src="https://www.centrinvest.ru/_ipx/_/logo.svg">
     </x-slot>
-    <div class = "mt-10 ml-10">
-        <div class="grid grid-rows-4 grid-flow-col gap-6">
-            <div class="row-span-4 bg-red-200">01</div>
-            <div class="col-span-3 bg-red-300">02</div>
-            <div class="row-span-3 col-span-2 bg-red-400">03</div>
-            <div class="row-span-4 col-span-2 bg-red-300">02</div>
+    <div class = "bg-gradient-to-br from-sky-900 via-blue-600 to-cyan-700 h-screen w-full ">
+        <div class = "flex item-center justify-center">
+            <input type = "text"
+                class = "placeholder:italic placeholder:text-green-600 rounded-md ml-2 w-1/2 text-xl h-16 mt-10 border border-green-600 text-blue-500"
+                placeholder="Введите свою фамилию"></input>
+            <input type = "text"
+                class = "placeholder:italic placeholder:text-green-600 w-1/2 rounded-md h-16 text-2xl ml-4 mt-10 border border-green-600 text-blue-500"
+                placeholder="Введите свою группу"></input>
+        </div>
+        <div class = "flex item-center justify-center">
+            <button
+                class="outline outline-offset-2 text-xl ml-10 font-medium outline-emerald-400 border mr-8 border-green-400 w-20 mt-10 bg-white text-green-600 rounded-md">Поиск</button>
+        </div>
+        <div
+            class = "grid place-content-center box-decoration-slice text-center mt-12 bg-gradient-to-r text-5xl from-indigo-600 to-pink-500 text-white px-2">
+            Ну привет, сталкер
+        </div>
+        <div class="flex flex-col">
+            <div class = "text-center form-textarea text-4xl text-red-700 font-sans">
+                Пожалуйста, заполни поля как написано выше!
+            </div>
         </div>
     </div>
 </div>
